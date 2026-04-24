a2122626bd10f701ed024cb2831769786c83e395
Nguyen Ngoc Tan <nn226509@gmail.com>
2026-04-24 11:35:24 +0700

    update code

--- src/scene/Home.tsx
+++ src/scene/Home.tsx
@@ -6,7 +6,7 @@ import {
   SpriteRender,
 } from '@safe-engine/webgl'
 
-import { sf_home, sf_button_start, sf_button_map, sf_button_exit, sf_button_game, sf_button_hoicham, sf_button_setting, sf_button_cup, sf_button_cart } from '../assets'
+import { sf_home, sf_button_start, sf_button_map, sf_button_exit, sf_button_game, sf_button_hoicham, sf_button_setting, sf_button_cup, sf_button_cart, sf_button_tw, sf_button_facebook, sf_button_gg, sf_button_info, sf_button_w } from '../assets'
 import { Scenes } from '../helper/constant'
 import { FighterScene } from './FighterScene'
 import { SpineScene } from './SpineScene'
@@ -50,72 +50,108 @@ export class Home extends SceneComponent {
       />
 
       <ButtonComp
-        node={{ xy: [970, 2220], scale: 0.35 }}
+        node={{ xy: [110, 2220], scale: 0.55 }}
+        spriteFrame={sf_button_info}
+        zoomScale={0.65}
+        onPress={this.onPress}
+      >
+        <ExtraDataComp key="id" value={Scenes.Fighter} />
+      </ButtonComp>
+
+      <ButtonComp
+        node={{ xy: [970, 2220], scale: 0.55 }}
         spriteFrame={sf_button_setting}
-        zoomScale={0.42}
+        zoomScale={0.65}
         onPress={this.onPress}
       >
         <ExtraDataComp key="id" value={Scenes.Fighter} />
       </ButtonComp>
 
       <ButtonComp
-        node={{ xy: [970, 2080], scale: 0.35 }}
+        node={{ xy: [970, 2040], scale: 0.55 }}
         spriteFrame={sf_button_game}
-        zoomScale={0.42}
+        zoomScale={0.65}
         onPress={this.onPress}
       >
         <ExtraDataComp key="id" value={Scenes.Fighter} />
       </ButtonComp>
 
       <ButtonComp
-        node={{ xy: [970, 1940], scale: 0.35 }}
+        node={{ xy: [970, 1860], scale: 0.55 }}
         spriteFrame={sf_button_cup}
-        zoomScale={0.42}
+        zoomScale={0.65}
         onPress={this.onPress}
       >
         <ExtraDataComp key="id" value={Scenes.Fighter} />
       </ButtonComp>
 
       <ButtonComp
-        node={{ xy: [970, 1800], scale: 0.35 }}
+        node={{ xy: [970, 1680], scale: 0.55 }}
         spriteFrame={sf_button_cart}
-        zoomScale={0.42}
+        zoomScale={0.65}
         onPress={this.onPress}
       >
         <ExtraDataComp key="id" value={Scenes.Fighter} />
       </ButtonComp>
 
       <ButtonComp
-        node={{ xy: [970, 1660], scale: 0.35 }}
-        spriteFrame={sf_button_hoicham}
-        zoomScale={0.42}
-        onPress={this.onPress}
-      >
-        <ExtraDataComp key="id" value={Scenes.Fighter} />
-      </ButtonComp>
-
-      <ButtonComp
-        node={{ xy: [540, 1080], scale: 0.8 }}
+        node={{ xy: [540, 1180], scale: 0.7 }}
         spriteFrame={sf_button_start}
-        zoomScale={0.92}
+        zoomScale={0.85}
         onPress={this.onPress}
       >
         <ExtraDataComp key="id" value={Scenes.Fighter} />
       </ButtonComp>
 
       <ButtonComp
-        node={{ xy: [540, 860], scale: 0.8 }}
+        node={{ xy: [540, 950], scale: 0.7 }}
         spriteFrame={sf_button_map}
-        zoomScale={0.92}
+        zoomScale={0.85}
         onPress={this.onPress}
       >
         <ExtraDataComp key="id" value={Scenes.Spine} />
       </ButtonComp>
 
       <ButtonComp
-        node={{ xy: [540, 640], scale: 0.8 }}
+        node={{ xy: [240, 150], scale: 0.55 }}
+        spriteFrame={sf_button_facebook}
+        zoomScale={0.65}
+        onPress={this.onPress}
+      >
+        <ExtraDataComp key="id" value={Scenes.Button} />
+      </ButtonComp>
+
+      <ButtonComp
+        node={{ xy: [440, 150], scale: 0.55 }}
+        spriteFrame={sf_button_tw}
+        zoomScale={0.65}
+        onPress={this.onPress}
+      >
+        <ExtraDataComp key="id" value={Scenes.Button} />
+      </ButtonComp>
+
+      <ButtonComp
+        node={{ xy: [640, 150], scale: 0.55 }}
+        spriteFrame={sf_button_w}
+        zoomScale={0.65}
+        onPress={this.onPress}
+      >
+        <ExtraDataComp key="id" value={Scenes.Button} />
+      </ButtonComp>
+
+      <ButtonComp
+        node={{ xy: [840, 150], scale: 0.55 }}
+        spriteFrame={sf_button_gg}
+        zoomScale={0.65}
+        onPress={this.onPress}
+      >
+        <ExtraDataComp key="id" value={Scenes.Button} />
+      </ButtonComp>
+
+      <ButtonComp
+        node={{ xy: [540, 720], scale: 0.7 }}
         spriteFrame={sf_button_exit}
-        zoomScale={0.92}
+        zoomScale={0.85}
         onPress={this.onPress}
       >
         <ExtraDataComp key="id" value={Scenes.Button} />
